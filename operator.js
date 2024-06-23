29a010f1d5da01759b2e5012cd3c7e67b76a4bb0
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2024-06-23 11:37:07 +0530

    first commit

---
+++ operator.js
@@ -0,0 +1,21 @@
+console.log("operators in javascript")
+a=15
+b=4
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a%b=",a%b)
+console.log("a**b=",a**b)
+console.log("a++=",a++)  // it will print 15
+console.log(a)   // it will print 16
+
+console.log("a++=",++a)   // in this case it will print 17
+console.log(a)  // it will print 17
+
+console.log("a--=",a--)
+console.log(a)
+
+console.log("--a=",--a)
+console.log(a)
+      
